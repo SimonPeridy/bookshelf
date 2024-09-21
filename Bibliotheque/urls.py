d3088161e56b23312c10a28a3b78e5d9b44ae43c
Levitan <Levitan@me>
2024-09-21 13:32:51 +0200

    Redirecting from / to bookshelv/

--- Bibliotheque/urls.py
+++ Bibliotheque/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import redirect
+from django.urls import include, path
 
 urlpatterns = [
+    path("", lambda req: redirect("bookshelv/")),
     path("bookshelv/", include("bookshelv.urls")),
     path("admin/", admin.site.urls),
 ]
